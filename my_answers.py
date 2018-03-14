d74f2b18b9a70698e215fcecf641f53568a17874
juangomez <juanbertgomez@gmail.com>
2018-03-14 21:59:18 +0100

    first commit

--- my_answers.py
+++ my_answers.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Activation
 from keras.layers import LSTM
+from keras import optimizers
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
-    y = []
+    y = series[window_size:]
 
-    # reshape each 
+    for i in range(len(series)-window_size):
+        X.append(series[i:i+window_size])
+
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
@@ -23,24 +27,38 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+
+    model = Sequential()
+    model.add(LSTM( 5, input_shape=(window_size,1)))
+    model.add(Dense(1))
+
+    # build model using keras documentation recommended optimizer initialization
+    #optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+    # compile the model
+    #model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
+    import re
     punctuation = ['!', ',', '.', ':', ';', '?']
-
-    return text
+    regular_expression = '[^a-zA-Z ' + ''.join(map(str,punctuation))+']+'
+    return re.sub(regular_expression, '', text)
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
+
     # containers for input/output pairs
     inputs = []
     outputs = []
-
+    for i in range(len(text)):
+        inputs.append(text[i: i + window_size])
+        outputs.append(text[i + window_size : i + window_size + step_size])
     return inputs,outputs
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+# TODO build the required RNN model:
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
 def build_part2_RNN(window_size, num_chars):
     pass
